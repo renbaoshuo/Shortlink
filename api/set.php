155688790fa91b08349c447ca301e644b5c7f7c8
任宝硕 <renbaoshuo@qq.com>
2020-03-09 23:58:16 +0800

    上传了一些文件

---
+++ api/set.php
@@ -0,0 +1,30 @@
+<?php
+    // 引入类
+    require_once('../inc/require.php');
+    global $config;
+    $url_c = new url();
+
+    $opt = [];
+    $opt['success'] = 'false';
+
+    $request_arr = json_decode(file_get_contents('php://input'), true);
+    if(isset($request_arr['url'])) {
+        // 添加 HTTP 协议前缀
+        if(!strstr($request_arr['url'], 'http://') && !strstr($request_arr['url'], 'https:')) $request_arr['url'] = 'http://' . $request_arr['url'];
+        // 检测网址格式是否正确
+        $is_link = preg_match('(http(|s)://([\w-]+\.)+[\w-]+(/)?)', $request_arr['url']);
+        // 判断条件
+        if($request_arr['url'] != '' && !strstr($request_arr['url'], $_SERVER['HTTP_HOST']) && $is_link) {
+            $opt['success'] = 'true';
+            $opt['content']['url'] = $url_c->set_url($request_arr['url'], $config['length']);
+        } else if(strstr($request_arr['url'], $_SERVER['HTTP_HOST'])) {
+            $opt['content'] = '链接已经是短地址了。';
+        } else if(!$is_link) {
+            $opt['content'] = '请输入正确格式的网址。';
+        }
+    } else {
+        $opt['content'] = '调用参数不能为空。';
+    }
+    // 输出
+    echo json_encode($opt);
+?>
---
+++ asset/js/app.js
@@ -0,0 +1,60 @@
+	
+function copyText() {
+        var input = document.getElementById("shorturl");
+        input.select(); // 选中文本
+        document.execCommand("copy"); // 执行浏览器复制命令
+}
+
+
+var APP = (function(){
+
+    var fn = {
+        
+        // 生成短地址
+        setUrl: function(self) {
+            var urlEl = document.getElementById('url'),
+                tips = 'https://',
+                request = {"url": urlEl.value};
+            fn.getJson('api/set.php', true, JSON.stringify(request), function(res) {
+                if(res.success == 'true') {
+                    //urlEl.className = 'focus';
+                    //urlEl.value = res.content.url;
+                    $res = document.getElementById('shorturl')
+                    $res.className = 'focus';
+                    $res.value = res.content.url;
+                } else {
+                    urlEl.className = '';
+                    urlEl.value = '';
+                    urlEl.setAttribute('placeholder', res.content);
+                    setTimeout(function() {
+                        urlEl.setAttribute('placeholder', tips);
+                    }, 2000);
+                }
+            });
+        },
+        // 获取 JSON 数据
+        getJson: function(url, post, data, callback) {
+            var xhr = new XMLHttpRequest(),
+                type = (post) ? 'POST' : 'GET';
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState == 4 && xhr.status == 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    callback(json);
+                } else if(xhr.readyState == 4) {
+                    callback(false);
+                }
+            }
+            xhr.open(type, url, true);
+            xhr.send(data);
+        } 
+    },
+    init = function() {
+        setTimeout(function() {
+            var el = document.getElementsByTagName('html')[0];
+            el.className = 'on';
+        }, 10);
+    };
+    return {fn: fn, init: init}
+})();
+
+document.addEventListener('DOMContentLoaded', function() {APP.init();})
